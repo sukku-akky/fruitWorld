d1508d63368d918cceb414ed920a8a995ebcb771
sukku-tech <cherukupallisukanya808gmail.com>
2024-06-26 03:48:55 +0530

    createElements

--- fruitWorld/index.js
+++ fruitWorld/index.js
@@ -1,6 +1,19 @@
-const listItem=document.getElementsByTagName("li");
-listItem[4].style.color="blue";
+const header=document.querySelector("#header")
+const mainHeading=document.querySelector("#main-heading");
+const head3=document.createElement("h3");
+const heading=document.createTextNode("Buy high quality organic fruits online");
+head3.appendChild(heading);
+header.appendChild(head3);
 
-for(let i=0;i<listItem.length;i++){
-    listItem[i].style.fontStyle="italic";
-};
+head3.style.fontStyle=("italic");
+
+const unorderedList=document.querySelector(".fruits");
+const para=document.createElement("p");
+const paraText=document.createTextNode("Total fruits: 4");
+para.appendChild(paraText);
+const divs=document.getElementsByTagName("div");
+const secondDiv=divs[1];
+secondDiv.insertBefore(para,unorderedList);
+
+para.id="fruits-total";
+
