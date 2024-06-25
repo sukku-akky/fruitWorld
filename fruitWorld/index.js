778625a49a179057bfdccba592682bd9bf417d87
sukku-tech <cherukupallisukanya808gmail.com>
2024-06-25 17:17:16 +0530

    firstUi

---
+++ fruitWorld/index.js
@@ -0,0 +1,10 @@
+const heading=document.getElementById("main-heading");
+heading.textContent="Fruit World";
+heading.style.color="orange";
+const header=document.getElementById("header");
+header.style.backgroundColor="green";
+header.style.borderBottom="3px solid orange";
+const basketHeading=document.getElementById("basket-heading");
+basketHeading.style.color="green";
+const paragraph=document.getElementById("thanks");
+paragraph.innerHTML="<p>Please visit us again</p>";
