2b4f72b934d4c374b132af7c07f345762cdf5eb7
sukku-tech <cherukupallisukanya808gmail.com>
2024-06-26 04:25:34 +0530

    addAndDeleteFunctionality

--- fruitWorld/index.js
+++ fruitWorld/index.js
@@ -1,6 +1,31 @@
-const listItem=document.getElementsByTagName("li");
-listItem[4].style.color="blue";
 
-for(let i=0;i<listItem.length;i++){
-    listItem[i].style.fontStyle="italic";
-};
+const listItem=document.querySelectorAll(".fruit");
+
+listItem.forEach(li=>{
+    const button=document.createElement("button");
+    button.textContent="Edit";
+    button.className="edit-btn";
+    
+
+    li.appendChild(button);
+});
+
+const form=document.querySelector("form");
+const fruits=document.querySelector(".fruits");
+form.addEventListener("click",function(event){
+  event.preventDefault();
+  const fruitToAdd=document.getElementById("fruit-to-add");
+  const newLi=document.createElement("li");
+  newLi.innerHTML=fruitToAdd.value+"<button class='delete-btn'>x</button><button class='edit-btn'>Edit</button>";
+ 
+
+
+  fruits.appendChild(newLi);
+})
+
+fruits.addEventListener("click",function(event){
+  if(event.target.classList.contains("delete-btn")){
+    const fruitToDelete=event.target.parentElement;
+    fruits.removeChild(fruitToDelete);
+  }
+})
