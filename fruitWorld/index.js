0deffeedcd17b5d8418b5b706dba18403dff6e17
sukku-tech <cherukupallisukanya808gmail.com>
2024-06-25 19:23:35 +0530

    querySelectors

--- fruitWorld/index.js
+++ fruitWorld/index.js
@@ -1,6 +1,29 @@
-const listItem=document.getElementsByTagName("li");
-listItem[4].style.color="blue";
 
-for(let i=0;i<listItem.length;i++){
-    listItem[i].style.fontStyle="italic";
-};
+const mainHeading=document.querySelector("#main-heading");
+mainHeading.style.textAlign="right";
+// Write answer to the questions asked below:
+const basketHeading=document.querySelector("#basket-heading");
+basketHeading.style.color="brown";
+basketHeading.style.margin="20px";
+
+const fruitItems=document.querySelector(".fruits");
+
+fruitItems.style.backgroundColor="grey";
+
+fruitItems.style.margin="20px";
+fruitItems.style.padding="20px";
+fruitItems.style.borderRadius="5px";
+fruitItems.style.listStyleType="none";
+const fruit=document.querySelectorAll(".fruit");
+for(let i=0;i<fruit.length;i++){
+  fruit[i].style.backgroundColor="white";
+  fruit[i].style.padding="10px";
+  fruit[i].style.margin="10px";
+  fruit[i].style.borderRadius="5px";
+}
+
+const evenFruit=document.querySelectorAll(".fruit:nth-child(even)");
+for(let i=0;i<evenFruit.length;i++){
+  evenFruit[i].style.backgroundColor="brown";
+  evenFruit[i].style.color="white";
+}
